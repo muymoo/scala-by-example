b0401a911cb88b902e21f121b63818ac8294595a
Momont, Matt (GE Global Research) <Matt.Momont@ge.com>
2016-12-05 10:43:55 -0800

    Update comments and function names for FirstClass functions

--- src/main/scala/org/matt/example/FirstClass.scala
+++ src/main/scala/org/matt/example/FirstClass.scala
@@ -5,7 +5,7 @@ object FirstClass {
   def productInts: (Int, Int) => Int = product(x => x)
 
   def product(f: Int => Int)(a: Int, b: Int): Int = {
-    computeOverRange((x, y) => x * y, f, 1)(a, b)
+    applyOperationToFunctionOverRange((x, y) => x * y, f, 1)(a, b)
   }
 
   def powerOfTwo(i: Int): Int = if (i == 0) 1 else 2 * powerOfTwo(i - 1)
@@ -17,13 +17,29 @@ object FirstClass {
   def sumInts: (Int, Int) => Int = sum(x => x)
 
   def sum(f: Int => Int)(a: Int, b: Int): Int = {
-    computeOverRange((x, y) => x + y, f, 0)(a, b)
+    applyOperationToFunctionOverRange((x, y) => x + y, f, 0)(a, b)
   }
 
-  def computeOverRange(op: (Int, Int) => Int, f: Int => Int, intial: Int)(a: Int, b: Int): Int = {
+  /**
+    * This should probably be called MapAndReduce.
+    *
+    * @example
+    * {{{
+    * def product(f: Int => Int)(a: Int, b: Int): Int = {
+    *   if(a>b) 1 else f(a) * product(f)(a+1, b)
+    * }
+    * }}}
+    * @param operation Operation used to combine results of function (i.e. reduce) Examples include [[FirstClass.sum]] and [[FirstClass.product]]
+    * @param function  The function to apply to each Int in range
+    * @param intial    Intial value that operation will start on (0 for sum, 1 for product)
+    * @param a         Lower bound
+    * @param b         Upper bound
+    * @return The result of applying the combining operation to the result of function applied to each value in the range.
+    */
+  def applyOperationToFunctionOverRange(operation: (Int, Int) => Int, function: Int => Int, intial: Int)(a: Int, b: Int): Int = {
     def iter(a: Int, result: Int): Int = {
       if (a > b) result
-      else iter(a + 1, op(result, f(a)))
+      else iter(a + 1, operation(result, function(a)))
     }
 
     iter(a, intial)
